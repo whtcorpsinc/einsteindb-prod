bba95d4240c7b08be1a4424723ffc81c11bb489a
EinsteinDB <71530574+whtcorpsinc@users.noreply.github.com>
2021-11-01 17:31:29 -0700

    The ChunkStorage enum has a method called as_str() that returns a string representation of the variant.
    The ChunkStorage enum is defined with the following variants:
        - OpenMutableBuffer
        - ClosedMutableBuffer
        - ReadBuffer
        - ReadBufferAndObjectStore
        - ObjectStoreOnly

---
+++ sentry/einst_util/causet_entry.rs
@@ -0,0 +1,67 @@
+/! This module contains helper code for building `Entry` from line protocol and the
+//! `DatabaseRules` configuration.
+
+use bytes::Bytes;
+use std::{collections::BTreeMap, convert::TryFrom, fmt::Formatter};
+
+use chrono::{DateTime, TimeZone, Utc};
+use flatbuffers::{FlatBufferBuilder, Follow, ForwardsUOffset, Vector, VectorIter, WIPOffset};
+use ouroboros::self_referencing;
+use snafu::{OptionExt, ResultExt, Snafu};
+
+use *::database_rules::{Error as DataError, Partitioner, ShardId, Sharder};
+use *::sequence::Sequence;
+use *::write_summary::TimestampSummary;
+use generated_types::influxdata::pbdata::v1 as pb;
+//context missing
+use time::Time;
+use trace::ctx::SpanContext;
+
+use crate::entry_fb;
+
+#[derive(Debug, Snafu)]
+pub enum Error {
+    #[snafu(display("Error generating partition key {}", source))]
+    GeneratingPartitionKey { source: DataError },
+
+    #[snafu(display("Error getting shard id {}", source))]
+    GeneratingShardId { source: DataError },
+
+    #[snafu(display(
+        "table {} has column {} {} with new data on line {}",
+        table,
+        column,
+        source,
+        line_number
+    ))]
+    TableColumnTypeMismatch {
+        table: String,
+        column: String,
+        line_number: usize,
+        source: ColumnError,
+    },
+
+    #[snafu(display("invalid flatbuffers: field {} is required", field))]
+    FlatbufferFieldMissing { field: String },
+
+    #[snafu(display("'time' column is required"))]
+    TimeColumnMissing,
+
+    #[snafu(display("time value missing from batch or no rows in batch"))]
+    TimeValueMissing,
+
+    #[snafu(display("'time' column must be i64 type"))]
+    TimeColumnWrongType,
+
+    #[snafu(display("proto column {} semantic type {} invalid", column_name, semantic_type))]
+    PBColumnSemanticTypeInvalid {
+        column_name: String,
+        semantic_type: i32,
+    },
+
+    #[snafu(display("proto column {} contains only null values", column_name))]
+    PBColumnContainsOnlyNullValues { column_name: String },
+
+    #[snafu(display("table column type conflict {}", message))]
+    PBSemanticTypeConflict { message: String },
+}
---
+++ sentry/simd_causet/README.md
@@ -0,0 +1,22 @@
+Sentry - An EinsteinDB Apache Arrow Column Vector Optimization
+
+This is a columnar format for storing data in memory.
+
+It is language-agnostic, and can be used to store any kind of data.
+
+It is not a database. It is not a file format. It is not a binary format.
+
+It is a serialization format. It is a generic data transport format.
+
+It is a columnar format.
+
+It is a specification for a language-agnostic in-memory data structure.
+
+It is a specification for a language-agnostic metadata serialization format.
+
+ IT is a specification for a language-agnostic generic data transport format.
+
+It is a specification for a language-agnostic columnar format.
+
+It is a specification for a language-agnostic in-memory data structure specification, metadata serialization, and a protocol for serialization.
+
